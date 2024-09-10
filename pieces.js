03e54f9ba1f972a4f9f8f5cf81eacc9dec242127
Damien Palm <palm.damien.pro@gmail.com>
2024-09-10 16:20:02 +0200

    partie 2 finie

--- pieces.js
+++ pieces.js
@@ -1,32 +1,97 @@
-const response = await fetch("pieces-autos.json");
-const piece = await response.json();
+// Récupère les pièces depuis le fichier JSON
+const pieces = await fetch("pieces-autos.json").then((pieces) => pieces.json());
 
-console.log(piece);
+// Fonction générant toute la page web
+function genererPieces(pieces) {
+  for (let i = 0; i < pieces.length; i++) {
+    const article = pieces[i];
+    // Récuperation de l'élément du DOM qui actualise les fiches
+    const sectionFiches = document.querySelector(".fiches");
+    // Création d'une balise dédiée à une piece auto
+    const pieceElement = document.createElement("article");
+    // Création des balises
+    const imageElement = document.createElement("img");
+    imageElement.src = article.image;
+    const nomElement = document.createElement("h2");
+    nomElement.innerText = article.nom;
+    const prixElement = document.createElement("p");
+    prixElement.innerText = `Prix : ${article.prix} € (${
+      article.prix < 35 ? "€" : "€€€"
+    })`;
+    const categorieElement = document.createElement("p");
+    categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
+    const descriptionElement = document.createElement("p");
+    descriptionElement.innerText =
+      article.description ?? "(Pas de description pour le moment)";
+    const stockElement = document.createElement("p");
+    stockElement.innerText = article.disponibilite ? "En stock" : "En rupture";
 
-const article = piece[0];
-const imageElement = document.createElement("img");
-imageElement.src = article.image;
-const nomElement = document.createElement("h2");
-nomElement.innerText = article.nom;
-const prixElement = document.createElement("p");
-prixElement.innerText = `Prix: ${article.prix} € (${
-  article.prix < 35 ? "€" : "€€€"
-})`;
-const categorieElement = document.createElement("p");
-categorieElement.innerText = article.categorie ?? "(aucune catégorie)";
-const descriptionElement = document.createElement("p");
-descriptionElement.innerText =
-  article.description ?? "(Pas de description pour le moment)";
-const stockElement = document.createElement("p");
-stockElement.innerText = article.disponibilite
-  ? "En stock"
-  : "En rupture de stock";
+    // On rattache la balise article à la balise fiches
+    sectionFiches.appendChild(pieceElement);
+    // On rattache les éléments composant la carte à la balise article
+    pieceElement.appendChild(imageElement);
+    pieceElement.appendChild(nomElement);
+    pieceElement.appendChild(prixElement);
+    pieceElement.appendChild(categorieElement);
+    pieceElement.appendChild(descriptionElement);
+    pieceElement.appendChild(stockElement);
+  }
+}
 
-const sectionFiches = document.querySelector(".fiches");
+// Premier affichage de la page
+genererPieces(pieces);
 
-sectionFiches.appendChild(imageElement);
-sectionFiches.appendChild(nomElement);
-sectionFiches.appendChild(prixElement);
-sectionFiches.appendChild(categorieElement);
-sectionFiches.appendChild(descriptionElement);
-sectionFiches.appendChild(stockElement);
+// Ajout du listener pour trier les pièces par ordre de prix croissant
+const boutonTrier = document.querySelector(".btn-trier");
+boutonTrier.addEventListener("click", function () {
+  // La méthode Array.from() permet de créer une nouvelle instance d'Array (une copie superficielle)
+  // à partir d'un objet itérable ou semblable à un tableau.
+  const piecesOrdonnees = Array.from(pieces);
+  piecesOrdonnees.sort(function (a, b) {
+    return b.prix - a.prix;
+  });
+  // Effacement de l'écran et regénération de la page
+  document.querySelector(".fiches").innerHTML = "";
+  genererPieces(piecesOrdonnees);
+});
+
+//Ajout du listener pour trier les pièces par ordre decroissant
+const boutonDecroissant = document.querySelector(".btn-decroissant");
+boutonDecroissant.addEventListener("click", function () {
+  const piecesOrdonnees = Array.from(pieces);
+  piecesOrdonnees.sort(function (a, b) {
+    return a.prix - b.prix;
+  });
+  document.querySelector(".fiches").innerHTML = "";
+  genererPieces(piecesOrdonnees);
+});
+
+// Ajout du listener pour filtrer les pièces non abordables
+const boutonFiltrer = document.querySelector(".btn-filtrer");
+boutonFiltrer.addEventListener("click", function () {
+  const piecesFiltrees = pieces.filter(function (piece) {
+    return piece.disponibilite;
+  });
+  // Effacement de l'écran et regénération de la page avec les pièces filtrées uniquement
+  document.querySelector(".fiches").innerHTML = "";
+  genererPieces(piecesFiltrees);
+});
+
+// du listener pour filtrer les pièces sans description
+const boutonNoDescription = document.querySelector(".btn-nodesc");
+boutonNoDescription.addEventListener("click", function () {
+  const piecesFiltrees = pieces.filter(function (piece) {
+    return piece.description;
+  });
+  document.querySelector(".fiches").innerHTML = "";
+  genererPieces(piecesFiltrees);
+});
+
+const inputPrixMax = document.querySelector("#prix-max");
+inputPrixMax.addEventListener("input", function () {
+  const piecesFiltrees = pieces.filter(function (piece) {
+    return piece.prix <= inputPrixMax.value;
+  });
+  document.querySelector(".fiches").innerHTML = "";
+  genererPieces(piecesFiltrees);
+});
